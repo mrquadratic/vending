c4d8021c156aef8f220040c8b35d0b2a2b8a435a
Jake <zamorejake@gmail.com>
2025-11-07 23:56:40 -0600

    finished files

--- vendingMachine.js
+++ vendingMachine.js
@@ -6,4 +6,23 @@ if (snackCount === null) {
 
   console.log("No snacks requested. Exiting.");
 
+} else {
+let totalSnack = 0
+for (let i = 1; i <= snackCount; i++) {
+
+  let snack = prompt("Enter snack #" + i + " name:");
+  
+  if (snack === null) {
+
+  console.log("Snack selection canceled. Exiting.");
+  console.log(`You got a total of ${totalSnack} snacks.`);
+  break;
+}
+console.log("Snack #" + i + ": " + snack + " given to customer");
+  totalSnack++
+  if (i % 3 === 0) {
+    totalSnack++
+    console.log(" You got a bonus snack!");
+}
+}
 }
